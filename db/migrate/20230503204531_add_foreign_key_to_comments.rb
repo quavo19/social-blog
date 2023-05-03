072850c1a7543177e12de5bb4d11b000af786224
Donald Akite <donaldakite27@gmail.com>
2023-05-03 21:25:40 +0000

    Add foreign keys

--- db/migrate/20230503204531_add_foreign_key_to_comments.rb
+++
@@ -1,6 +0,0 @@
-class AddForeignKeyToComments < ActiveRecord::Migration[7.0]
-  def change
-    t.references :author, foreign_key: { to_table: 'users' }
-    t.references :post, foreign_key: true
-  end
-end
--- db/migrate/20230503205029_add_foreign_key_to_like.rb
+++
@@ -1,6 +0,0 @@
-class AddForeignKeyToLike < ActiveRecord::Migration[7.0]
-  def change
-    t.references :author foreign_key: {to_table: 'users'}
-    t.references :post foreign_key: true
-  end
-end

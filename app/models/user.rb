daba9ffc31a9e299a0fa95c8a05d2a7bf321ac9b
Donald Akite <donaldakite27@gmail.com>
2023-05-05 09:40:43 +0000

    Add method to get recent posts of a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
     has_many :likes, dependent: :destroy
     has_many :posts, dependent: :destroy
     has_many :comments, dependent: :destroy
+
+    private 
+
+    def recent_posts
+        posts.order(created_at: :desc).limit(3)
+    end
 end

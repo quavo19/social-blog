450a3255abc901d9c043ce66f6d0e19bed021993
Donald Akite <donaldakite27@gmail.com>
2023-05-05 17:35:31 +0000

    Add tests for comments

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,13 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  describe '#update_comments_count' do
-    let(:post) { Post.create(title: 'Test Post') }
-    let(:author) { User.create(name: 'Test Author') }
-    let(:comment) { Comment.create(body: 'Test Comment', post: post, author: author) }
+  let(:user) { User.create(name: 'Donald', posts_counter: 0) }
+  let(:post) { user.posts.create(title: 'My Post', comments_counter: 0, likes_counter: 0) }
+  let(:comment) { post.comments.create(author_id: user.id) }
 
-    it 'updates the comments_counter attribute of the associated post' do
-      expect { comment.send(:update_comments_count) }.to change { post.reload.comments_counter }.by(1)
-    end
+  it 'increase the number of comments after a comment' do
+    expect { comment.save! }.to change { post.reload.comments_counter }.from(0).to(1)
   end
 end

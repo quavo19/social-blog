7cf535abfd8afd18b9f038f7b5157698b9d9fb0b
Donald Akite <donaldakite27@gmail.com>
2023-05-05 11:38:51 +0000

    Fix naming error

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,10 @@
 class Comment < ApplicationRecord
     belongs_to :post
-    belongs_to :author class_name: 'User'
+    belongs_to :author, class_name: 'User'
     after_save :update_comments_count
 
     private
     def update_comments_count
-        post.update(comments_counte: post.comments.count)
+        post.update(comments_counter: post.comments.count)
     end
 end

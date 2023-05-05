1cce65c8b65a555f942027af2ddc76ba0ea1ff0c
Donald Akite <donaldakite27@gmail.com>
2023-05-05 19:23:09 +0000

    Add post test for recent post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -22,4 +22,17 @@ RSpec.describe Post, type: :model do
       end
     end
   end
+  describe '#recent_comments' do
+    let(:post) { Post.create(title: 'Post 1', author_id: user.id, comments_counter: 0, likes_counter: 0) }
+    let!(:comment1) { Comment.create(post_id: post.id, author_id: user.id, created_at: 1.hour.ago) }
+    let!(:comment2) { Comment.create(post_id: post.id, author_id: user.id, created_at: 30.minutes.ago) }
+    let!(:comment3) { Comment.create(post_id: post.id, author_id: user.id, created_at: 5.minutes.ago) }
+    let!(:comment4) { Comment.create(post_id: post.id, author_id: user.id, created_at: 10.minutes.ago) }
+    let!(:comment5) { Comment.create(post_id: post.id, author_id: user.id, created_at: 2.hours.ago) }
+    let!(:comment6) { Comment.create(post_id: post.id, author_id: user.id, created_at: 1.minute.ago) }
+
+    it 'returns the 5 most recent comments' do
+      expect(post.recent_comments.pluck(:id)).to eq([comment6.id, comment3.id, comment4.id, comment2.id, comment1.id])
+    end
+  end
 end

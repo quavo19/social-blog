b98810c454135bd7c1c47d56429e53fc658ae5f0
Donald Akite <donaldakite27@gmail.com>
2023-05-05 09:39:10 +0000

    Add method to get recent posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,4 +10,8 @@ class Post < ApplicationRecord
         author.update(posts_counter: author.posts.count)
     end
 
+    def recent_comments
+        comments.order(created_at: :desc).limit(5)
+    end
+
 end

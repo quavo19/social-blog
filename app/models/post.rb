d79d50110f8d576690b9bd380969fa8ac9d46dbd
Donald Akite <donaldakite27@gmail.com>
2023-05-05 16:35:36 +0000

    Add validatiosn for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 250, message: 'title must not be greater than 250 characters' }
+  validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :likes, class_name: 'Like', foreign_key: 'post_id'
   has_many :comments, class_name: 'Comment', foreign_key: 'post_id'

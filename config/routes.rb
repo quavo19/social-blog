122a5424091eb91ee928dd058748919be5df86d1
Donald Akite <donaldakite27@gmail.com>
2023-05-09 09:56:12 +0000

    generate index and show routes only

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :users do
+  resources :users, only:[:index, :show] do
     resources :posts, only:[:index, :show]
   end
   root "users#index"

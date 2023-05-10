6eb7784e083883d98fcd30696e66881b1cc857a1
Donald Akite <donaldakite27@gmail.com>
2023-05-10 23:08:03 +0000

    Fix naming

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,13 @@
 class PostsController < ApplicationController
   def index
-    @user = Users.find(
+    @user = User.find(
     params[:user_id]
     )
     @posts = @user.posts
   end
 
   def show;
-    @user = Users.find(params[:user_id])
+    @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
   end
 end

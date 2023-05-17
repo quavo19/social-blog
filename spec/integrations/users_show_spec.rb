5654c24d4f3216ea2b62ed2a335cce719310b83c
Donald Akite <donaldakite27@gmail.com>
2023-05-17 18:10:07 +0000

    Add test to Redirect to show all users post

--- spec/integrations/users_show_spec.rb
+++ spec/integrations/users_show_spec.rb
@@ -9,7 +9,9 @@ RSpec.describe 'Users', type: :system, js: true do
                                 comments_counter: 0)
       @second_post = Post.create(author: @user, title: 'second title', text: 'text for second title', likes_counter: 0,
                                  comments_counter: 0)
-      @third_post = Post.create(author: @user, title: 'last post', text: 'text for last post', likes_counter: 0,
+      @third_post = Post.create(author: @user, title: 'third post', text: 'text for last post', likes_counter: 0,
+                                comments_counter: 0)
+      @forth_post = Post.create(author: @user, title: 'last post', text: 'text for last post', likes_counter: 0,
                                 comments_counter: 0)
       visit user_path(@user)
     end
@@ -31,14 +33,21 @@ RSpec.describe 'Users', type: :system, js: true do
     end
 
     it "renders the user's last 3 posts" do
-      expect(page).to have_content(@first_post.title)
+      expect(page).to have_content(@forth_post.title)
       expect(page).to have_content(@second_post.title)
       expect(page).to have_content(@third_post.title)
     end
 
     it "redirects to user's posts page" do
-      click_link @first_post.title
-      expect(page).to have_current_path(user_post_path(@user, @first_post))
+      click_link @forth_post.title
+      expect(page).to have_current_path(user_post_path(@user, @forth_post))
+    end
+    it 'Redirect to show all users post' do
+      click_button 'see more'
+      expect(page).to have_content(@first_post.title)
+      expect(page).to have_content(@second_post.title)
+      expect(page).to have_content(@third_post.title)
+      expect(page).to have_content(@forth_post.title)
     end
   end
 end

50dea7dd66d6c8d3f44c1abadfffd136a3930979
Choice Osobor <choiceosobor@gmail.com>
2023-05-20 20:04:48 +0100

    create routes for API

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+  
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :users, only: %i[index show] do
+        resources :posts, only: %i[index] do
+          resources :comments, only: %i[index create]
+        end
+      end
+    end
+  end
   resources :users, only: [:index, :show] do
     resources :posts, except: [:update, :edit] do
       resources :comments, only: [:new, :create, :destroy]
